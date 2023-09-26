88b2d1198036bc1aa3c1903c43ac25b3848b99c3
SinnerSS <ntnbusinf@gmail.com>
2023-09-26 16:19:08 +0700

    add todo-comments keybind to which-key setup Signed-off-by: SinnerSS <ntnbusinf@gmail.com>

--- lua/plugins/helper/which-key.lua
+++ lua/plugins/helper/which-key.lua
@@ -1,14 +1,23 @@
 local wk = require("which-key")
 local builtin = require('telescope.builtin')
+local todo = require('todo-comments')
 wk.register({
   ["<leader>"] = {
     f = {
-      name = "+file";
+      name = "+find",
       f = {builtin.find_files, "Find File"},
       g = {builtin.live_grep, "Find String"},
       b = {builtin.buffers, "Find Buffer"},
       h = {builtin.help_tags, "Find Help Tag"}
     }
+  },
+  ["]"] = {
+    name = "+next",
+    t = {todo.jump_next, "Next todo comment"},
+  },
+  ["["] = {
+    name = "+previous",
+    t = {todo.jump_prev, "Previous todo comment"}
   },
   ["<C-\\>"] = {"<cmd>ToggleTerm<cr>", "Toggle Terminal"},
   ["<C-n>"] = {"<cmd>NvimTreeFindFileToggle<cr>", "Toggle File Explorer"},

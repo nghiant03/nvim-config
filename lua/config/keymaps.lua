5a3e9f94d8a23b8db80638166ddca9c05d22de80
Sinner <ntnbusinf@gmail.com>
2025-01-23 00:58:58 +0700

    add cursor movement to jump list in normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -36,6 +36,8 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
+-- Add cursor click to jump list
+keymap('n', '<LeftMouse>', "m'<LeftMouse>", opts)
 -- Insert --
 -- Press jk fast to exit insert mode 
 keymap("i", "jk", "<ESC>", opts)

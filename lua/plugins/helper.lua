eb7b5470085201ae6416adf2790e9d0c9d90676a
Sinner <ntnbusinf@gmail.com>
2025-02-20 21:20:19 +0700

    fix uninitialized autopair

--- lua/plugins/helper.lua
+++ lua/plugins/helper.lua
@@ -64,6 +64,7 @@ return {
     dependencies = 'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     config = function()
+      require('nvim-autopairs').setup{}
       local cmp_autopairs = require('nvim-autopairs.completion.cmp')
       local cmp = require('cmp')
       cmp.event:on(

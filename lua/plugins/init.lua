7d1b5b12e2c62c976d8e7f7cdd60b1151c91e514
Sinner <nghiant@mailbox.org>
2025-09-23 10:03:51 +0700

    add remote nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,4 +27,13 @@ return {
     build = ':Cord update',
     -- opts = {}
   },
+	{
+		'amitds1997/remote-nvim.nvim',
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		config = true
+	}
 }

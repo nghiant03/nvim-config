af4f018069f73c7a2fd182b0a84429795296d461
Sinner <nghiant@mailbox.org>
2025-11-12 18:02:39 +0700

    auto open window when open an ssh session

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,6 +34,27 @@ return {
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
 		},
+		opts = {
+			remote = {
+				copy_dirs = {
+					config = {
+						base = vim.fs.joinpath(vim.fn.expand("$XDG_CONFIG_HOME"), "remote-nvim"),
+					}
+				}
+			},
+			client_callback = function(port, _)
+				vim.notify("Opening Kitty…", vim.log.levels.INFO, { title = "remote-nvim" })
+
+				local cmd = ("kitty -e nvim --server localhost:%s --remote-ui"):format(port)
+				vim.fn.jobstart(cmd, {
+					detach = true,
+					on_exit = function(job_id, exit_code, event_type)
+						-- This function will be called when the job exits
+						print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+					end,
+				})
+			end,
+		},
 		config = true
 	}
 }

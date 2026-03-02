7393c76422191eea183669895310db56d289eb28
nghiant <nghiant@mailbox.org>
2026-03-02 21:49:30 +0700

    add local bin to path

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 vim.g.is_ssh = os.getenv("SSH_TTY") ~= nil
+if vim.g.is_ssh then
+	vim.env.PATH = vim.env.HOME .. "/.local/bin:" .. vim.env.PATH
+end
 require("config.options")
 require("config.keymaps")
 require("config.cmds")

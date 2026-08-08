51d814718cf38ef03e89c4f4bbff1cfdce18c2dd
nghiant <nghiant@mailbox.org>
2026-08-08 14:14:50 +0800

    remove minute ai plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,38 +1,4 @@
 return {
-  {
-    "milanglacier/minuet-ai.nvim",
-		dependencies = "nvim-lua/plenary.nvim",
-    event = "InsertEnter",
-    opts = {
-      provider = "codestral",
-      add_single_line_entry = false,
-      provider_options = {
-        codestral = {
-          optional = {
-            max_tokens = 256,
-            stop = { "\n\n" },
-          },
-        },
-        gemini = {
-          optional = {
-            generationConfig = {
-              maxOutputTokens = 256,
-            },
-            safetySettings = {
-              {
-                -- HARM_CATEGORY_HATE_SPEECH,
-                -- HARM_CATEGORY_HARASSMENT
-                -- HARM_CATEGORY_SEXUALLY_EXPLICIT
-                category = "HARM_CATEGORY_DANGEROUS_CONTENT",
-                -- BLOCK_NONE
-                threshold = "BLOCK_ONLY_HIGH",
-              },
-            },
-          },
-        },
-      },
-    },
-  },
 	{
 		"zbirenbaum/copilot.lua",
 		requires = {
